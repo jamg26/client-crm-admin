f8d0e73b955c3998537281744899c0d63fa4de7a
Joel Ralph Balignasay <joelralph06@gmail.com>
2020-03-20 16:53:10 +0800

    changes

--- src/app/pages/home/business/BusinessComponent.js
+++ src/app/pages/home/business/BusinessComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import BusinessViewComponents from './BusinessViewComponents';
+import BusinessViewComponents from './components/BusinessViewComponents';
 export default function BusinessComponent() {
     return (
         <Switch>
